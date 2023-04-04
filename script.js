4438dc77acc92d49c4ece00f82fdaa449a29afd7
mark mwendwa <mwendwamark310@gmail.com>
2023-04-04 11:28:55 +0300

    Second commit

--- script.js
+++ script.js
@@ -1,4 +1,23 @@
 
+const characterButton = document.querySelector("button[type='menu']");
+const characterList = ["Murray", "Eleven", "Hopper", "Jonathan", "Joyce", "Steve", "Mike", "Dustin", "Nancy", "Will","Erica"];
+let characterIndex = 0;
+
+characterButton.addEventListener("click", () => {
+  if (characterIndex < characterList.length) {
+    const character = characterList[characterIndex];
+    if (!document.querySelector(`#quote-list li[data-character='${character}']`)) {
+      const li = document.createElement("li");
+      li.textContent = character;
+      li.dataset.character = character;
+      document.querySelector("#quote-list").appendChild(li);
+    }
+    characterIndex++;
+  }
+});
+
+
+
 document.addEventListener('DOMContentLoaded', () => {
 
   
@@ -72,7 +91,43 @@ document.addEventListener('DOMContentLoaded', () => {
 fetchQuotes();
 
 
-})
+
+// Add the form submit event to add a new quote
+const form = document.querySelector('form');
+form.addEventListener('submit', (e) => {
+  e.preventDefault(); 
+  const quoteInput = document.querySelector('#quote');
+  const authorInput = document.querySelector('#author');
+  const newQuote = {
+    quote: quoteInput.value,
+    author: authorInput.value
+  };
+  addQuote(newQuote);
+  quoteInput.value = '';
+  authorInput.value = '';
+});
+
+// Handle the input change event to update a quote
+const ul = document.querySelector('#quote-list');
+ul.addEventListener('change', (event) => {
+  const quoteId = event.target.dataset.id;
+  const quoteField = event.target.dataset.field;
+  const updatedQuote = {};
+  updatedQuote[quoteField] = event.target.value;
+  updateQuote(quoteId, updatedQuote);
+});
+
+// Handle the button click event to delete a quote
+ul.addEventListener('click', (event) => {
+  if (event.target.tagName === 'BUTTON') {
+    const quoteId = event.target.parentElement.dataset.id;
+    deleteQuote(quoteId);
+    event.target.parentElement.remove();
+  }
+});
+
+});
+
 
 
   
