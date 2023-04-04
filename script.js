7cb840eb5098e326f7e64d305f34976ad57ca566
mark mwendwa <mwendwamark310@gmail.com>
2023-04-04 21:06:32 +0300

    second trial

--- script.js
+++ script.js
@@ -1,133 +1,77 @@
+// const quoteList = document.getElementById('quote-list');
+// const addQuoteForm = document.getElementById('add-quote-form');
 
-const characterButton = document.querySelector("button[type='menu']");
-const characterList = ["Murray", "Eleven", "Hopper", "Jonathan", "Joyce", "Steve", "Mike", "Dustin", "Nancy", "Will","Erica"];
-let characterIndex = 0;
+// function renderQuote(quote) {
+//   const li = document.createElement('li');
+//   const header = document.createElement('h2');
+//   const quoteText = document.createElement('p');
+//   const deleteButton = document.createElement('button');
 
-characterButton.addEventListener("click", () => {
-  if (characterIndex < characterList.length) {
-    const character = characterList[characterIndex];
-    if (!document.querySelector(`#quote-list li[data-character='${character}']`)) {
-      const li = document.createElement("li");
-      li.textContent = character;
-      li.dataset.character = character;
-      document.querySelector("#quote-list").appendChild(li);
-    }
-    characterIndex++;
-  }
-});
+//   header.textContent = quote.author;
+//   quoteText.textContent = quote.text;
+//   deleteButton.textContent = 'Delete';
+//   deleteButton.addEventListener('click', () => {
+//     deleteQuote(quote.id);
+//     alert(`Quote "${quote.text}" by ${quote.author} has been deleted.`);
+//   });
 
+//   li.appendChild(header);
+//   li.appendChild(quoteText);
+//   li.appendChild(deleteButton);
 
+//   quoteList.appendChild(li);
+// }
 
-document.addEventListener('DOMContentLoaded', () => {
+// function renderQuotes(quotes) {
+//   quoteList.innerHTML = '';
+//   quotes.forEach((quote) => {
+//     renderQuote(quote);
+//   });
+// }
 
-  
-  const fetchQuotes = () => {
-    fetch('http://localhost:3000/quotes')
+// function addQuote(author, text) {
+//   fetch('http://localhost:3000/quotes',
+//  {
+//     method: 'POST',
+//     headers: {
+//       'Content-Type': 'application/json'
+//     },
+//     body: JSON.stringify({
+//       author: author,
+//       text: text
+//     })
+//   })
+//   .then(response => response.json())
+//   .then(() => {
+//     fetchQuotes();
+//   });
+// }
 
-      .then(response => response.json())
-      .then(data => {
-      
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error.message);
-      });
-  };
+// function deleteQuote(id) {
+//   fetch(`http://localhost:3000/quotes ${id}`, {
+//     method: 'DELETE'
+//   })
+//   .then(response => response.json())
+//   .then(() => {
+//     fetchQuotes();
+//   });
+// }
 
-  //Adding a new quote to the server
-  const addQuote = (newQuote) => {
-    fetch('http://localhost:3000/quotes', {
-      method: 'POST',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(newQuote)
-    })
-      .then(response => response.json())
-      .then(data => {
-      
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error);
-      });
-  };
+// function fetchQuotes() {
+//   fetch('http://localhost:3000/quote')
+//   .then(response => response.json())
+//   .then(quotes => {
+//     renderQuotes(quotes);
+//   });
+// }
 
-  // Updating a quote on the server
-  const updateQuote = (quoteToUpdate, updatedQuote) => {
-    fetch(`http://localhost:3000/quotes/${quoteToUpdate}`, {
-      method: 'PATCH',
-      headers: {
-        'Content-Type': 'application/json'
-      },
-      body: JSON.stringify(updatedQuote)
-    })
-      .then(response => response.json())
-      .then(data => {
-      
-        console.log(data);
-      })
-      .catch(error => {
-        console.log(error.message);
-      });
-  };
+// addQuoteForm.addEventListener('submit', (event) => {
+//   event.preventDefault();
+//   const authorInput = addQuoteForm.elements.author;
+//   const textInput = addQuoteForm.elements.text;
+//   addQuote(authorInput.value, textInput.value);
+//   authorInput.value = '';
+//   textInput.value = '';
+// });
 
-  //Removing a quote from the server
-  const deleteQuote = (quoteToDelete) => {
-    fetch(`http://localhost:3000/quotes/${quoteToDelete}`, {
-      method: 'DELETE'
-    })
-      .then(response =>  response.json())
-      .then(data =>{
-        console.log(data)
-      })
-
-      .catch(error => {
-        console.error('Error deleting quote:', error);
-      });
-  };
-
-  
-fetchQuotes();
-
-
-
-// Add the form submit event to add a new quote
-const form = document.querySelector('form');
-form.addEventListener('submit', (e) => {
-  e.preventDefault(); 
-  const quoteInput = document.querySelector('#quote');
-  const authorInput = document.querySelector('#author');
-  const newQuote = {
-    quote: quoteInput.value,
-    author: authorInput.value
-  };
-  addQuote(newQuote);
-  quoteInput.value = '';
-  authorInput.value = '';
-});
-
-// Handle the input change event to update a quote
-const ul = document.querySelector('#quote-list');
-ul.addEventListener('change', (event) => {
-  const quoteId = event.target.dataset.id;
-  const quoteField = event.target.dataset.field;
-  const updatedQuote = {};
-  updatedQuote[quoteField] = event.target.value;
-  updateQuote(quoteId, updatedQuote);
-});
-
-// Handle the button click event to delete a quote
-ul.addEventListener('click', (event) => {
-  if (event.target.tagName === 'BUTTON') {
-    const quoteId = event.target.parentElement.dataset.id;
-    deleteQuote(quoteId);
-    event.target.parentElement.remove();
-  }
-});
-
-});
-
-
-
-  
+// fetchQuotes();
