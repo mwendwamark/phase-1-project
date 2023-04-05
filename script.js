7363355c2d4c785bb7a1c1acf2a27344ecb9d5db
mark mwendwa <mwendwamark310@gmail.com>
2023-04-05 16:08:35 +0300

    third trial

--- script.js
+++ script.js
@@ -1,95 +1,174 @@
 const quoteList = document.querySelector('#quote-list');
-
-async function getQuotes() {
-  const response = await fetch('http://localhost:3000/quotes');
-  const quotes = await response.json();
-  return quotes;
-}
-
-async function addQuote(author, quote) {
-  await fetch('http://localhost:3000/quotes', {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json'
+const quotes = {
+  "quotes": [
+    {
+      "quote": "Friends don't lie",
+      "author": "Eleven",
+      "id": 1,
+      "likes": 0
     },
-    body: JSON.stringify({ author, quote, likes: 0 })
-  });
-  const newQuote = await response.json();
-  return newQuote;
-}
-
-async function updateQuote(id, likes) {
-  const response = await fetch(`http://localhost:3000/quotes/${id}`, {
-    method: 'PATCH',
-    headers: {
-      'Content-Type': 'application/json'
+    {
+      "quote": "Mornings are for coffee and contemplation.?",
+      "author": "Hopper",
+      "id": 2,
+      "likes": 0
     },
-    body: JSON.stringify({ likes })
-  });
-  const updatedQuote = await response.json();
-  return updatedQuote;
-}
+    {
+      "quote": "If anyone asks where I am, I've left the country.",
+      "author": "Mike",
+      "id": 3,
+      "likes": 0
+    },
+    {
+      "quote": "You shouldn't like things because people tell you you're supposed to.",
+      "author": "Jonathan",
+      "id": 4,
+      "likes": 0
+    },
+    {
+      "quote": "Nobody normal ever accomplished anything meaningful in this world",
+      "author": "Jonathan",
+      "id": 5,
+      "likes": 0
+    },
+    {
+      "quote": "I am on a curiosity voyage, and I need my paddles to travel. These books…these books are my paddles",
+      "author": "Dustin",
+      "id": 6,
+      "likes": 0
+    },
+    {
+      "quote": "Science is neat, but I'm afraid it's not very forgiving.",
+      "author": "Mr Clarke",
+      "id": 7,
+      "likes": 0
+    },
+    {
+      "quote": "Ask for forgiveness, not permission",
+      "author": "Nancy",
+      "id": 8,
+      "likes": 0
+    },
+    {
+      "quote": "You keep trying things on until something feels like you.",
+      "author": "Max",
+      "id": 9,
+      "likes": 0
+    },
+    {
+      "quote": "Use the shampoo and conditioner and when your hair’s damp, not wet, okay? When it’s damp, you do four puffs of the Farrah Fawcett spray.",
+      "author": "Steve",
+      "id": 10,
+      "likes": 0
+    },
+    {
+      "quote": "You can't spell America wihtout Erica",
+      "author": "Erica",
+      "id": 11,
+      "likes": 0
+    },
+    {
+      "quote": "My fingers are like arrows! My arms, like iron! My feet, like spears! Resist, and I will end you",
+      "author": "Murry",
+      "id": 12,
+      "likes": 0
+    },
+    {
+      "quote": "I need this phone and two weeks' advance. And a pack of Camels",
+      "author": "Joyce",
+      "id": 13,
+      "likes": 0
+    },
+    {
+      "quote": "When you’re different, sometimes you feel like a mistake. But you make her feel like she’s not a mistake at all. Like she’s better for being different.",
+      "author": "Will",
+      "id": 14,
+      "likes": 0
+    }
+  ]
+};
 
-async function deleteQuote(id) {
-  const response = await fetch(`http://localhost:3000/quotes/${id}`, {
-    method: 'DELETE'
-  });
-  const deletedQuote = await response.json();
-  return deletedQuote;
-}
 
 function renderQuote(quote) {
-  const li = document.createElement('li');
-  li.innerHTML = `
-    <h3>${quote.author}</h3>
-    <p>${quote.quote}</p>
-    <button class="like-button" data-id="${quote.id}">&#x1F44D;</button>
-    <span class="like-counter">${quote.likes}</span>
-    <button class="delete-button" data-id="${quote.id}">Delete</button>
-  `;
-  quoteList.appendChild(li);
+
+  const quoteContainer = document.createElement('li');
+  quoteContainer.classList.add('quote-card');
+  quoteContainer.dataset.id = quote.id;
+
+  const quoteText = document.createElement('blockquote');
+  quoteText.classList.add('blockquote');
+
+  const quoteQuote = document.createElement('p');
+  quoteQuote.classList.add('mb-0');
+  quoteQuote.textContent = quote.quote;
+
+  const quoteAuthor = document.createElement('footer');
+  quoteAuthor.classList.add('blockquote-footer');
+  quoteAuthor.textContent = quote.author;
+
+  const likeBtn = document.createElement('button');
+  likeBtn.classList.add('btn', 'btn-success');
+  likeBtn.textContent = `Likes: ${quote.likes}`;
+
+  quoteText.appendChild(quoteQuote);
+  quoteText.appendChild(quoteAuthor);
+  quoteContainer.appendChild(quoteText);
+  quoteContainer.appendChild(likeBtn);
+
+  quoteList.appendChild(quoteContainer);
 }
 
-async function renderQuotes() {
-  quoteList.innerHTML = '';
-  const quotes = await getQuotes();
-  quotes.forEach(renderQuote);
-}
-
-async function handleSubmit(event) {
-  event.preventDefault();
-  const authorInput = document.querySelector('#author');
-  const quoteInput = document.querySelector('#quote');
-  const author = authorInput.value;
-  const quote = quoteInput.value;
-  await addQuote(author, quote);
-  authorInput.value = '';
+function addQuote(e) {
+  e.preventDefault();
+  const quoteInput = document.querySelector('#quote-input');
+  const authorInput = document.querySelector('#author-input');
+  const quote = {
+    quote: quoteInput.value,
+    author: authorInput.value,
+    id: quotes.quotes.length + 1,
+    likes: 0
+  };
+  quotes.quotes.push(quote);
+  renderQuote(quote);
   quoteInput.value = '';
-  await renderQuotes();
+  authorInput.value = '';
 }
 
-async function handleDelete(event) {
-  if (event.target.classList.contains('delete-button')) {
-    const id = event.target.dataset.id;
-    await deleteQuote(id);
-    await renderQuotes();
-  }
-}
+quotes.quotes.forEach(quote => {
+  
+});
 
-async function handleLike(event) {
-  if (event.target.classList.contains('like-button')) {
-    const id = event.target.dataset.id;
-    const quote = await updateQuote(id, parseInt(event.target.nextElementSibling.innerText) + 1);
-    event.target.nextElementSibling.innerText = quote.likes;
-  }
-}
-
-const form = document.querySelector('form');
-form.addEventListener('submit', handleSubmit);
-
-quoteList.addEventListener('click', handleDelete);
-quoteList.addEventListener('click', handleLike);
-
-(async function init() {
-  await renderQuotes();
-})();
+const addQuoteForm = document.querySelector('form');
+addQuoteForm.addEventListener('submit', addQuote);
+quotes.quotes.forEach(quote => {
+    const quoteContainer = document.createElement('li');
+    quoteContainer.classList.add('quote-card');
+    quoteContainer.dataset.id = quote.id;
+  
+    const quoteText = document.createElement('blockquote');
+    quoteText.classList.add('blockquote');
+  
+    const quoteQuote = document.createElement('p');
+    quoteQuote.classList.add('mb-0');
+    quoteQuote.textContent = quote.quote;
+  
+    const quoteAuthor = document.createElement('footer');
+    quoteAuthor.classList.add('blockquote-footer');
+    quoteAuthor.textContent = quote.author;
+  
+    const likeBtn = document.createElement('button');
+    likeBtn.classList.add('btn', 'btn-success');
+    likeBtn.textContent = `Likes: ${quote.likes}`;
+    likeBtn.addEventListener('click', () => {
+      quote.likes++;
+      likeBtn.textContent = `Likes: ${quote.likes}`;
+    });
+  
+    quoteText.appendChild(quoteQuote);
+    quoteText.appendChild(quoteAuthor);
+    quoteContainer.appendChild(quoteText);
+    quoteContainer.appendChild(likeBtn);
+  
+    quoteList.appendChild(quoteContainer);
+  });
+  
