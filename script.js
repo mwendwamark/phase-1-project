ad0cf9fe8d2f2ba840597c95bfd8069febb2318d
mark mwendwa <mwendwamark310@gmail.com>
2023-04-05 19:08:29 +0300

    almost done

--- script.js
+++ script.js
@@ -3,85 +3,89 @@ const quotes = {
   "quotes": [
     {
       "quote": "Friends don't lie",
-      "author": "Eleven",
+      "author": "ELEVEN",
       "id": 1,
-      "likes": 0
+      "likes": 0,
+      
     },
     {
       "quote": "Mornings are for coffee and contemplation.?",
-      "author": "Hopper",
+      "author": "HOPPER",
       "id": 2,
-      "likes": 0
+      "likes": 0,
+      
     },
     {
       "quote": "If anyone asks where I am, I've left the country.",
-      "author": "Mike",
+      "author": "MIKE",
       "id": 3,
-      "likes": 0
+      "likes": 0,
+      "url":""
     },
     {
       "quote": "You shouldn't like things because people tell you you're supposed to.",
-      "author": "Jonathan",
+      "author": "ROBIN",
       "id": 4,
-      "likes": 0
+      "likes": 0,
+      
     },
     {
       "quote": "Nobody normal ever accomplished anything meaningful in this world",
-      "author": "Jonathan",
+      "author": "JONATHAN",
       "id": 5,
       "likes": 0
     },
     {
       "quote": "I am on a curiosity voyage, and I need my paddles to travel. These books…these books are my paddles",
-      "author": "Dustin",
+      "author": "DUSTIN",
       "id": 6,
       "likes": 0
     },
     {
       "quote": "Science is neat, but I'm afraid it's not very forgiving.",
-      "author": "Mr Clarke",
+      "author": "MR. CLARKE",
       "id": 7,
       "likes": 0
     },
     {
       "quote": "Ask for forgiveness, not permission",
-      "author": "Nancy",
+      "author": "NANCY",
       "id": 8,
       "likes": 0
     },
     {
       "quote": "You keep trying things on until something feels like you.",
-      "author": "Max",
+      "author": "MAX",
       "id": 9,
       "likes": 0
     },
     {
+      "author": "STEVE",
       "quote": "Use the shampoo and conditioner and when your hair’s damp, not wet, okay? When it’s damp, you do four puffs of the Farrah Fawcett spray.",
-      "author": "Steve",
       "id": 10,
       "likes": 0
     },
     {
+      "author": "ERICA",
       "quote": "You can't spell America wihtout Erica",
-      "author": "Erica",
       "id": 11,
       "likes": 0
     },
     {
+      "author": "MURRAY",
       "quote": "My fingers are like arrows! My arms, like iron! My feet, like spears! Resist, and I will end you",
-      "author": "Murry",
       "id": 12,
       "likes": 0
     },
     {
+      "author": "JOYCE",
       "quote": "I need this phone and two weeks' advance. And a pack of Camels",
-      "author": "Joyce",
       "id": 13,
       "likes": 0
     },
     {
+      "author": "WILL",
       "quote": "When you’re different, sometimes you feel like a mistake. But you make her feel like she’s not a mistake at all. Like she’s better for being different.",
-      "author": "Will",
       "id": 14,
       "likes": 0
     }
@@ -170,5 +174,7 @@ quotes.quotes.forEach(quote => {
     quoteContainer.appendChild(likeBtn);
   
     quoteList.appendChild(quoteContainer);
+
+    
   });
   
